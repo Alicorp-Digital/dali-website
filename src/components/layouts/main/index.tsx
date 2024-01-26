fe810617ba420cefdf1cdee5e3f44d44977a6807
Paulo Tejada <paulotejada@MacBook-Air-de-Paulo.local>
2024-01-26 07:06:19 -0500

    feat(dali-site): typographypage

--- src/components/layouts/main/index.tsx
+++ src/components/layouts/main/index.tsx
@@ -1,59 +1,57 @@
-'use client';
+"use client";
 import { FC, ReactNode } from "react";
 import { Header } from "components";
-import './styles.scss';
+import "./styles.scss";
 import { DlSidebar } from "@alicorpdigital/dali-react-sidebar";
 
 type Props = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 const LayoutMain: FC<Props> = (props) => {
   const { children } = props;
 
   const items = [
     {
-      label: 'Item 1',
-      key: 'item1',
+      label: "Item 1",
+      key: "item1",
       // icon: <DlIcon name='package' />,
       children: [
-        {label: 'subItem 1', key: 'subItem1'},
-        {label: 'subItem 2', key: 'subItem2'},
+        { label: "subItem 1", key: "subItem1" },
+        { label: "subItem 2", key: "subItem2" },
       ],
     },
     {
-      label: 'Item 2',
-      key: 'item2',
+      label: "Item 2",
+      key: "item2",
       // icon: <DlIcon name='package' />,
       children: [
-        {label: 'subItem 1', key: 'subItem11'},
-        {label: 'subItem 2', key: 'subItem22'},
+        { label: "subItem 1", key: "subItem11" },
+        { label: "subItem 2", key: "subItem22" },
       ],
     },
     {
-      label: 'Item 3',
-      key: 'item3',
+      label: "Item 3",
+      key: "item3",
       // icon: <DlIcon name='package' />,
       disabled: true,
     },
     {
-      label: 'Item 4',
-      key: 'item4',
+      label: "Item 4",
+      key: "item4",
       // icon: <DlIcon name='package' />,
-    }
-  ]
+    },
+  ];
   return (
-    <div className='dali-layout-main'>
+    <div className="dali-layout-main">
       <Header />
-      <div className='dali-layout-main__content'>
+      <div className="dali-layout-main__content">
         <DlSidebar items={items} />
 
-        <div className='dali-layout-main__children'>
-          {children}
-        </div>
+        <div className="dali-layout-main__children">{children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default LayoutMain;
