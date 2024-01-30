1bd94fbbb21a41dd0dc5268aef7020ded9782af1
iLTW1n <jose.osk01@gmail.com>
2024-01-30 04:51:28 -0500

    feat(first-steps): add type for map

---
+++ src/toolbox/data-sources/main.data-source.ts
@@ -0,0 +1,16 @@
+import { Size } from 'types/main.type';
+
+export const sizes: Size[] = [
+  {
+    size: 'small',
+    text: 'Small'
+  },
+  {
+    size: 'medium',
+    text: 'Medium'
+  },
+  {
+    size: 'large',
+    text: 'Large'
+  }
+]
--- src/toolbox/types/main.type.ts
+++ src/toolbox/types/main.type.ts
@@ -1,4 +1,4 @@
-export type Scale = {
+export type Size = {
   size: string;
   text: string;
 }
